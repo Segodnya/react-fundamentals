358ba63a00df7c2e31e49aeebe12960df1361adc
Kirill Kurentsov <immortalsteel@gmail.com>
2023-03-02 06:11:28 +0300

    feat: add page count

--- src/App.js
+++ src/App.js
@@ -12,16 +12,22 @@ import { usePosts } from "./hooks/usePosts.js";
 import { useFetching } from "./hooks/useFetching.js";
 import PostService from "./API/PostService.js";
 import Loader from "./components/UI/Loader/Loader.jsx";
+import { getPageCount } from "./utils/pages.js";
 
 function App() {
   const [posts, setPosts] = useState([]);
   const [filter, setFilter] = useState({ sort: "", query: "" });
   const [modal, setModal] = useState(false);
+  const [totalPages, setTotalPages] = useState(0);
+  const [limit, setLimit] = useState(10);
+  const [page, setPage] = useState(1);
   const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
 
   const [fetchPosts, isPostsLoading, postError] = useFetching(async () => {
-    const posts = await PostService.getAll();
-    setPosts(posts);
+    const response = await PostService.getAll(limit, page);
+    setPosts(response.data);
+    const totalCount = response.headers["x-total-count"];
+    setTotalPages(getPageCount(totalCount, limit));
   });
 
   useEffect(() => {
