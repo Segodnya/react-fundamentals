005581b9b95da440db9c83c9b8a932cfaff9764a
Kirill Kurentsov <immortalsteel@gmail.com>
2023-03-04 05:50:48 +0300

    feat: add comments page for each post

--- src/API/PostService.js
+++ src/API/PostService.js
@@ -17,4 +17,26 @@ export default class PostService {
       console.log(e);
     }
   }
+
+  static async getById(id) {
+    try {
+      const response = await axios.get(
+        "https://jsonplaceholder.typicode.com/posts/" + id
+      );
+      return response;
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  static async getCommentsByPostId(id) {
+    try {
+      const response = await axios.get(
+        `https://jsonplaceholder.typicode.com/posts/${id}/comments`
+      );
+      return response;
+    } catch (e) {
+      console.log(e);
+    }
+  }
 }
--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -3,12 +3,14 @@ import { Route, Routes } from "react-router-dom";
 import About from "../pages/About.jsx";
 import Posts from "../pages/Posts.jsx";
 import Error from "../pages/Error.jsx";
+import PostIdPage from "../pages/PostIdPage.jsx";
 
 const AppRouter = () => {
   return (
     <Routes>
       <Route path="/about" element={<About />} />
-      <Route path="/posts" element={<Posts />} />
+      <Route exact path="/posts" element={<Posts />} />
+      <Route exact path="/posts/:id" element={<PostIdPage />} />
       <Route path="*" element={<Error />} />
     </Routes>
   );
